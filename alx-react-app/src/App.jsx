9472ded1ba704f222fa021b4ef13c2a16aa5c911
Thendo m <mbengenithendo46@gmail.com>
2025-11-16 11:52:15 +0200

    1st

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,5 @@
+<WelcomeMessage />  
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,6 +10,8 @@ function App() {
 
   return (
     <>
+ <WelcomeMessage />
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
