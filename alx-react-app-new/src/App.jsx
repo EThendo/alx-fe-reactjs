ab5d01a4698e30957cdb42b89b5ae441988f640a
Thendo m <mbengenithendo46@gmail.com>
2025-11-19 12:22:47 +0200

    hey

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,30 +1,13 @@
 import React from "react";
-import Header from "./components/Header";
-import UserProfile from "./components/UserProfile";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
-      {/* Header with inline styling */}
-      <Header />
-
-      {/* UserProfile with inline styling */}
-      <UserProfile
-        name="Thendo Mbengeni"
-        age={23}
-        bio="Media & Tech enthusiast"
-      />
-
-      {/* MainContent with inline styling */}
-      <MainContent />
-
-      {/* Footer with inline styling */}
-      <Footer />
+      <h1>Simple Counter App</h1>
+      <Counter />
     </div>
   );
 }
 
 export default App;
-
